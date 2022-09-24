dbc53c23bc6b151ba8132792920711da0086ed69
test_2022 <prudkyi87@gmail.com>
2022-09-24 21:54:45 +0200

    git 1.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 import * as flsFunctions from "./modules/functions.js";
+import * as jqFunctions from "./modules/jquery_functions.js";
+
 flsFunctions.isWebP();
 /* next Working */
+$(document).ready(function() {
+
+
+
+});
